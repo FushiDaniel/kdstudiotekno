9d664ee6572abb0a9dfcc14dca48588d3947179b
aimanniel <hello@aimandaniel.my>
2025-07-05 02:28:00 +0800

    fix: update configuration for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
+    unoptimized: true,
   },
   reactStrictMode: true,
   devIndicators: {
     buildActivity: false,
   },
+  output: 'standalone',
+  swcMinify: true,
 }
 
 module.exports = nextConfig 
